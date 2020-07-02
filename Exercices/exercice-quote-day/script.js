648879fecca17627f89226f4ab1ac64085f4ad65
Joéllie Sprumont <joellie@siriusschool.be>
2020-07-02 16:05:30 +0200

    Update-javascript

--- Exercices/exercice-quote-day/script.js
+++ Exercices/exercice-quote-day/script.js
@@ -1,1 +1,21 @@
 /* C'est ici qu'il faut rédiger ton code pour utiliser l'API */
+const getRandomQuotes = () => {
+  let randomNumber = Math.floor(Math.random() * Math.floor(1643));
+  fetch('https://type.fit/api/quotes')
+    .then(response => {
+      return response.json();
+    })
+    .then(response => {
+      let data = response[randomNumber];
+      document.getElementById('quotes').textContent = data.text;
+      document.getElementById('author').textContent = data.author;
+    });
+};
+getRandomQuotes();
+
+function buttonListener() {
+  document.getElementById('container').addEventListener('click', () => {
+    getRandomQuotes();
+  });
+}
+buttonListener();
